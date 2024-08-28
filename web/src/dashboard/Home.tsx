77cf797d5db05ad9e819640fadedfda0c6d528a7
Kurt Angeles <kurttck7034@gmail.com>
2024-08-27 22:07:26 -0500

    [UPDATE] Agregamos bootstrap

---
+++ web/src/dashboard/Home.tsx
@@ -0,0 +1,14 @@
+/* interface props {
+  children: JSX.Element;
+  de: string;
+} */
+
+function Home(/* { children, de }: props */) {
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  );
+}
+
+export default Home;
