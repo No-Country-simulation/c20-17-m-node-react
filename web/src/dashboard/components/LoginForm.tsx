98115d95644a4b5f86b53952bcf3c10c0baace55
Kurt Angeles <kurttck7034@gmail.com>
2024-08-29 00:48:08 -0500

    [UPDATE] Ysa chambea aqui

--- web/src/dashboard/components/LoginForm.tsx
+++ web/src/dashboard/components/LoginForm.tsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+/* 
+<h1>Hola</h1>
+        <Link to="/dashboard" state={{ name: "Kurt" }}>
+          Ingresar
+        </Link> */
 
 function LoginForm() {
   return (
     <>
       <div className="container-fluid h-100 p-0 d-flex flex-column align-items-center justify-content-center text-bg-dark">
-        <h1>Hola</h1>
-        <Link to="/dashboard" state={{ name: "Kurt" }}>
-          Ingresar
-        </Link>
+        <div>CHAMBEAR AQUI</div>
       </div>
     </>
   );
