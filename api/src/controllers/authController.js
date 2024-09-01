d77f797990ef8e929cbf909859566fd067fc9c2c
WalterM <waltermaza@gmail.com>
2024-09-01 20:51:23 -0300

    Update auth controller

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -11,18 +11,16 @@ export const generateToken = (id) => {
 
 //registerUser
 export const registerUser = async (req, res) => {
-    const user = req.body
+    const data = req.body
 
     try {
-        const userExists = await User.findOne({email:user.email})
+        const userExists = await User.findOne({email:data.email})
 
         if(userExists) {
             res.status(401).json({message: 'User already exists.'})
         }
 
-        const newUser = await User.create(user)
-
-        // res.status(201).json(newUser)
+        const user = await User.create(data)
 
         res.status(201).json({
             _id: user._id,
@@ -34,12 +32,19 @@ export const registerUser = async (req, res) => {
         res.status(500).json({message: err.message})
     }
 
-
 }
 
 //loginUser
 export const loginUser = async (req, res) => {
+    const user = req.body
 
+    try {
+        
+
+
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 }
 
 //getMe
