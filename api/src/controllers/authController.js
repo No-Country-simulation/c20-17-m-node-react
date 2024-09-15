da2d91db81bef3541f02ed9e8a91d6f6df885bc2
WalterM <waltermaza@gmail.com>
2024-09-14 21:33:39 -0300

    Add generate alias in register

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -10,9 +10,44 @@ export const generateToken = (id) => {
   });
 };
 
+//generateAlias
+export const generateAlias = (first_name, last_name) => {
+  let alias;
+
+  function generateRandomString(length) {
+    // const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789-.';
+
+
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+
+    }
+    return result;
+  }
+
+  if (first_name && last_name) {
+    // alias = `${first_name.charAt(0)}.${last_name.substring(0, Math.min(last_name.length, 8))}`;
+    alias = `${first_name.charAt(0)}${last_name.substring(0, Math.min(last_name.length, 8))}.noabank`;
+
+    //agregamos caracteres aleatorios hasta completar la longitud deseada
+    while (alias.length < 6) {
+      alias += generateRandomString(1);
+    }
+  } else {
+    alias = `${first_name.substring(0, Math.min(first_name.length, 8))}.noabank`;
+    //agregamos caracteres aleatorios hasta completar la longitud deseada
+    while (alias.length < 6) {
+      alias += generateRandomString(1);
+    }
+  }
+  // return alias;
+  return alias.toLowerCase()
+}
+
 //registerUser
 export const registerUser = async (req, res) => {
-  console.log("entramos al api registerUser");
   const data = req.body;
 
   try {
@@ -30,6 +65,7 @@ export const registerUser = async (req, res) => {
     const user = await User.create({
       ...data,
       account_type: accountType,
+      alias: generateAlias(data.first_name, data.last_name),
     });
 
     res.status(201).json({
