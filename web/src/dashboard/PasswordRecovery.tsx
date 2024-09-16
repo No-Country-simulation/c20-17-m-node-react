88b95a6392867693c941343145ffd2d1e5da8906
Kurt Angeles <kurttck7034@gmail.com>
2024-09-16 18:08:35 -0500

    [UPDATE] EMAIL RECOVERY PASSWORD

--- web/src/dashboard/PasswordRecovery.tsx
+++ web/src/dashboard/PasswordRecovery.tsx
@@ -1,7 +1,18 @@
 import "./css/PasswordRecovery.css";
 import img from "../assets/logo.png";
+import { useState } from "react";
+import { forgotpassword } from "../services/authService";
 
 function PasswordRecovery() {
+  const [passwordrecovery, setPasswordRecovery] = useState("");
+
+  const handlePasswordRecovery = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const messageRecovery = await forgotpassword(passwordrecovery);
+
+    alert(messageRecovery.message);
+  };
+
   return (
     <>
       <div className="containerPasswordRecovery">
@@ -13,16 +24,25 @@ function PasswordRecovery() {
             <h1>Olvidaste tu contraseña</h1>
           </div>
           <div className="recoveryForm">
-            <form action="">
+            <form action="" onSubmit={handlePasswordRecovery}>
               <div className="recoveryInput">
                 <p>
                   Ingresa tu correo electronico y te enviaremos un correo para
                   que resetees tu contraseña
                 </p>
-                <input type="email" id="email" className="forminput" />
+                <input
+                  type="email"
+                  id="email"
+                  className="forminput"
+                  value={passwordrecovery}
+                  onChange={(e) => setPasswordRecovery(e.target.value)}
+                />
               </div>
               <button type="submit">Enviar</button>
             </form>
+            <div>
+              <form action=""></form>
+            </div>
           </div>
         </div>
       </div>
