da46ef3722d4a723c7226cd5b6ec2781c6edc732
Kurt Angeles <kurttck7034@gmail.com>
2024-09-17 20:40:46 -0500

    [update] admin actualizado

--- api/src/utils/transferData.js
+++ api/src/utils/transferData.js
@@ -1,60 +1,60 @@
-import Transfer from "../models/transfer";
+import Transfer from "../models/transfer.js";
 
 //funcion crea transfers:
 export const transfers = async (user) => {
-    return await Transfer.aggregate([
-      {
-        $match: {
-          $or: [{ emisor_id: user._id }, { receptor_id: user._id }],
+  return await Transfer.aggregate([
+    {
+      $match: {
+        $or: [{ emisor_id: user._id }, { receptor_id: user._id }],
+      },
+    },
+    {
+      $sort: { createdAt: -1 },
+    },
+    {
+      $limit: 10,
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "emisor_id",
+        foreignField: "_id",
+        as: "emisor",
+      },
+    },
+    {
+      $unwind: "$emisor",
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "receptor_id",
+        foreignField: "_id",
+        as: "receptor",
+      },
+    },
+    {
+      $unwind: "$receptor",
+    },
+
+    {
+      $project: {
+        _id: 1,
+        mount: 1,
+        createdAt: {
+          $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
+        },
+        emisor: {
+          firstname: "$emisor.first_name",
+          lastname: "$emisor.last_name",
+          emisorId: "$emisor._id",
+        },
+        receptor: {
+          firstname: "$receptor.first_name",
+          lastname: "$receptor.last_name",
+          receptorId: "$receptor._id",
         },
       },
-      {
-        $sort: { createdAt: -1 },
-      },
-      {
-        $limit: 10,
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "emisor_id",
-          foreignField: "_id",
-          as: "emisor",
-        },
-      },
-      {
-        $unwind: "$emisor",
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "receptor_id",
-          foreignField: "_id",
-          as: "receptor",
-        },
-      },
-      {
-        $unwind: "$receptor",
-      },
-  
-      {
-        $project: {
-          _id: 1,
-          mount: 1,
-          createdAt: {
-            $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
-          },
-          emisor: {
-            firstname: "$emisor.first_name",
-            lastname: "$emisor.last_name",
-            emisorId: "$emisor._id",
-          },
-          receptor: {
-            firstname: "$receptor.first_name",
-            lastname: "$receptor.last_name",
-            receptorId: "$receptor._id",
-          },
-        },
-      },
-    ]);
-  }
+    },
+  ]);
+};
